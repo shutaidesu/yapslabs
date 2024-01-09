b03e4cf7732c47df45ebe5cc4973d4926f92ea01
shutaidesu <shutaidesu@mail.ru>
2024-01-09 17:36:21 +0300

    lab3

---
+++ lab3/Catalog.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace lab3
+{
+    class Catalog
+    {
+        private List<Composition> compositions = new List<Composition>();
+
+        public void AddComposition(Composition composition)
+        {
+            // Проверка наличия композиции с таким ID в списке
+            while (compositions.Any(c => c.Id == composition.Id))
+            {
+                composition = new Composition(); // Увеличиваем ID, если уже существует композиция с таким ID
+            }
+
+            compositions.Add(composition);
+        }
+
+        public void RemoveComposition(Composition composition)
+        {
+            compositions.Remove(composition);
+        }
+
+        public List<Composition> Search(string criteria)
+        {
+            // Пример простого поиска по критерию в названии или исполнителе
+            return compositions.FindAll(c =>
+                c.Artist.Contains(criteria, StringComparison.OrdinalIgnoreCase) ||
+                c.Title.Contains(criteria, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public List<Composition> Search(string artistCriteria, string TitleCriteria)
+        {
+            // Пример простого поиска по критерию в названии или исполнителе
+            return compositions.FindAll(c =>
+                c.Artist.Contains(artistCriteria, StringComparison.OrdinalIgnoreCase) ||
+                c.Title.Contains(TitleCriteria, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void DisplayAllCompositions()
+        {
+            if (compositions.Count > 0)
+            {
+                Console.WriteLine("Все композиции в каталоге:");
+                foreach (var composition in compositions)
+                {
+                    Console.WriteLine(composition);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Каталог пуст");
+            }
+        }
+
+        public void ExportToSql()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                var allContacts = dbContext.Compositions.ToList();
+                dbContext.Compositions.RemoveRange(allContacts);
+                dbContext.SaveChanges();
+
+                dbContext.Compositions.AddRange(compositions);
+                dbContext.SaveChanges();
+            }
+        }
+        public void ExportToJson()
+        {
+            string jsonString = JsonSerializer.Serialize(compositions);
+            File.WriteAllText("Composition.json", jsonString);
+        }
+        public void ExportToXml()
+        {
+            XmlSerializer mySerializer = new XmlSerializer(typeof(List<Composition>));
+            StreamWriter myWriter = new StreamWriter("Composition.xml");
+            mySerializer.Serialize(myWriter, compositions);
+            myWriter.Close();
+            Console.WriteLine("Данные успешно записаны в xml");
+        }
+        public void ImportFromJson()
+        {
+            string jsonString = File.ReadAllText("Composition.json");
+            compositions = JsonSerializer.Deserialize<List<Composition>>(jsonString);
+            Console.WriteLine($"Данные успешно считаны из файла JSON: Composition.json");
+        }
+        public void ImportFromXml()
+        {
+            XmlSerializer mySerializer = new XmlSerializer(typeof(List<Composition>));
+            using var myFileStream = new FileStream("Composition.xml", FileMode.Open);
+            compositions = (List<Composition>)mySerializer.Deserialize(myFileStream);
+        }
+        public void ImportFromSql()
+        {
+            using (var dbContext = new AppDbContext())
+            {
+                compositions = dbContext.Compositions.ToList();
+
+            }
+        }
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace lab3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Добро пожаловать в музыкальный каталог!");
+            Console.WriteLine("Доступные команды:");
+            Console.WriteLine("1. Поиск композиции");
+            Console.WriteLine("2. Вывод информации о всех композициях");
+            Console.WriteLine("3. Добавление композиции");
+            Console.WriteLine("4. Удаление композиции");
+            Console.WriteLine("5. Экспортировать в JSON");
+            Console.WriteLine("6. Экспортировать в XML");
+            Console.WriteLine("7. Экспортировать в SQLite");
+            Console.WriteLine("8. Имортировать из JSON");
+            Console.WriteLine("9. Имортировать из XML");
+            Console.WriteLine("10. Имортировать из SQLite");
+            Console.WriteLine("11. Выход");
+
+            Catalog catalog = new Catalog();
+
+            while (true)
+            {
+                Console.Write("Введите номер команды: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Введите критерий поиска: ");
+                        string searchCriteria = Console.ReadLine();
+                        List<Composition> searchResults = catalog.Search(searchCriteria);
+                        Console.WriteLine("Результаты поиска:");
+                        foreach (var result in searchResults)
+                        {
+                            Console.WriteLine(result);
+                        }
+                        break;
+
+                    case "2":
+                        catalog.DisplayAllCompositions();
+                        break;
+
+                    case "3":
+                        Console.Write("Введите имя исполнителя: ");
+                        string artist = Console.ReadLine();
+                        Console.Write("Введите название композиции: ");
+                        string title = Console.ReadLine();
+
+                        Composition newComposition = new Composition { Artist = artist, Title = title };
+                        catalog.AddComposition(newComposition);
+                        Console.WriteLine("Композиция добавлена в каталог.");
+                        break;
+
+                    case "4":
+                        Console.Write("Введите имя исполнителя композиции для удаления: ");
+                        string artistToDelete = Console.ReadLine();
+                        Console.Write("Введите название композиции для удаления: ");
+                        string titleToDelete = Console.ReadLine();
+
+                        Composition compositionToDelete = catalog.Search(artistToDelete, titleToDelete).FirstOrDefault();
+
+                        if (compositionToDelete != null)
+                        {
+                            catalog.RemoveComposition(compositionToDelete);
+                            Console.WriteLine("Композиция удалена из каталога.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Композиция не найдена в каталоге.");
+                        }
+                        break;
+                    case "5":
+                        catalog.ExportToJson();
+                        break;
+                    case "6":
+                        catalog.ExportToXml();
+                        break;
+                    case "7":
+                        catalog.ExportToSql();
+                        break;
+                    case "8":
+                        catalog.ImportFromJson();
+                        break;
+                    case "9":
+                        catalog.ImportFromXml();
+                        break;
+                    case "10":
+                        catalog.ImportFromSql();
+                        break;
+
+                    case "11":
+                        Console.WriteLine("Выход из программы.");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректная команда. Пожалуйста, введите корректный номер команды.");
+                        break;
+                }
+            }
+        }
+    }
+}
