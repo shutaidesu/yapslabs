8a8ee37651008d98f994151cb444cf46a7448f19
shutaidesu <shutaidesu@mail.ru>
2024-01-09 17:32:46 +0300

    lab1

---
+++ lab1v6/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace lab1v6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            FirstPart firstPart = new FirstPart();
+            Console.Write("Введите размер массива N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] array = firstPart.GenerateRandomArray(N);
+
+            Console.WriteLine("Сгенерированный массив:");
+            firstPart.PrintArray(array);
+
+            int maxIndex = firstPart.FindMaxIndex(array);
+            Console.WriteLine($"Номер максимального элемента: {maxIndex + 1}");
+
+            int firstZeroIndex = Array.IndexOf(array, 0);
+            int secondZeroIndex = Array.IndexOf(array, 0, firstZeroIndex + 1);
+
+            if (firstZeroIndex == -1 || secondZeroIndex == -1)
+            {
+                Console.WriteLine("В массиве не хватает нулевых элементов для вычисления произведения.");
+            }
+            else
+            {
+                int product = firstPart.CalculateProductBetweenZeros(array, firstZeroIndex, secondZeroIndex);
+                Console.WriteLine($"Произведение элементов между первым и вторым нулевыми элементами: {product}");
+            }
+
+            int[] transformedArray = firstPart.TransformArray(array);
+            Console.WriteLine("Преобразованный массив:");
+            firstPart.PrintArray(transformedArray);
+
+            // ========================================== 2 часть =========================================
+
+            SecondPart secondPart = new SecondPart();
+
+            int[,] matrix = secondPart.GenerateMatrix(8, 8);
+
+            Console.WriteLine("Исходная матрица:");
+            secondPart.PrintMatrix(matrix);
+
+            secondPart.FindMatchingRowAndColumn(matrix);
+            secondPart.SumRowsWithNegativeElement(matrix);
+        }
+    }
+}
---
+++ lab1v6/SecondPart.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1v6
+{
+    public class SecondPart
+    {
+        public int[,] GenerateMatrix(int rows, int columns)
+        {
+            int[,] matrix = new int[rows, columns];
+            Random random = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = random.Next(-10, 11);
+                }
+            }
+
+            return matrix;
+        }
+
+        public void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public void FindMatchingRowAndColumn(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            Console.WriteLine("Строки, совпадающие с соответствующими столбцами:");
+
+            for (int k = 0; k < rows; k++)
+            {
+                bool matchFound = true;
+
+                for (int i = 0; i < columns; i++)
+                {
+                    if (matrix[k, i] != matrix[i, k])
+                    {
+                        matchFound = false;
+                        break;
+                    }
+                }
+
+                if (matchFound)
+                {
+                    Console.WriteLine($"Строка {k + 1} совпадает с столбцом {k + 1}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        public void SumRowsWithNegativeElement(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            int sum = 0;
+
+            Console.WriteLine("Сумма элементов в строках с хотя бы одним отрицательным элементом:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                bool hasNegativeElement = false;
+
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        hasNegativeElement = true;
+                        break;
+                    }
+                }
+
+                if (hasNegativeElement)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        sum += matrix[i, j];
+                    }
+
+                    Console.WriteLine($"Сумма элементов в строке {i + 1}: {sum}");
+                    sum = 0;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+}
